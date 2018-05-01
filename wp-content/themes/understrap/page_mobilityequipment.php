dc08be5fcb227678a3c2c91753d980bf228d2c25
Grey <greymcgowan@gmail.com>
2018-05-01 15:49:00 +1000

    template for the mobility aids now shows all of the fields (completely unformatted) issue #8 also seems to have immediately fixed itself.

--- wp-content/themes/understrap/page_mobilityequipment.php
+++ wp-content/themes/understrap/page_mobilityequipment.php
@@ -3,10 +3,7 @@
  * Template Name: Mobility Equipment
  * The template for displaying all pages.
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * This is the template that displays all mobility equipment.
  *
  * @package understrap
  */
@@ -16,44 +13,41 @@ get_header();
 $container   = get_theme_mod( 'understrap_container_type' );
 
 ?>
+
+<!-- page content -->
 <div id="mobilityaidpage" class="content-area">
 		<h1 id='aidh' class='entry-title'> Mobility Aids </h1>
 		<main id="aidm" class="site-folio">
 			<?php
-			// query parameters
-			$args = array(
-				'post_type' => 'mobilityaid',
-				'orderby' => 'date',
-				'order' => 'DESC'
-			);
-			$folio = new WP_Query($args);
+			//sets up the parameters for the query that calls upon the CPT
+				$args = array(
+					'post_type' => 'mobilityaid',
+					'orderby' => 'menu_order',
+					'order' => 'ASC'
+				);
+				// creates the query and saves the content as $mobilityaid
+				$mobilityaid = new WP_Query($args);
 			?>
-			
-			
-			<?php 
-			//display content
-			if( $mobilityaid->have_posts() ): ?>
-				<?php while( $mobilityaid->have_posts() ): $mobilityaid->the_post(); ?>
-					<article class="mobilityaid">
-						<div class="aid_header">
-						<a href='<?php the_permalink(); ?>'>	
+			<?php
+				//the loop that repeats it for each post and displays it on the page
+				if ( $mobilityaid->have_posts() ):
+					while( $mobilityaid->have_posts() ) : $mobilityaid->the_post(); ?>
+						<article class="mobilityaidpost">
 							<h2><?php the_title(); ?></h2>
-						</a>
-						</div>
-						<div class='aid-content'>
-						<a href='<?php the_permalink(); ?>'>		
-							<?php the_post_thumbnail(); ?>
-						</a>
+							<?php the_field('first_purchase_type'); ?>
+							<?php the_field('first_cost'); ?>
+							<?php the_field('second_purchase_type'); ?>
+							<?php the_field('second_cost'); ?>
 							<?php the_content(); ?>
-						</div>
-					</article>
-				<?php endwhile; ?>
-			<?php else: ?>
-				<p>Sorry, there are no projects to be found. </p>
-			<?php endif; ?>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+							<?php the_field('in_stock'); ?>
+						</article>
+					<?php endwhile; ?>
+				<?php else: ?>
+					<p> Sorry, we cant find any mobility equipment to list, please call us </p>
+				<?php endif; ?>
+		</main>
 
 <?php
 get_sidebar();
 get_footer();
+?>
