ba727598f3464a9aa22252ee2821067d02fe4532
Grey <greymcgowan@gmail.com>
2018-05-06 16:47:38 +1000

    Removed normalisation as it's provided by bootstrap Started styling the colours & nav Added the sans font (nunito sans) Added a list of colours used attempted issue #17

--- wp-content/themes/szwarcbardpharmacy/header.php
+++ wp-content/themes/szwarcbardpharmacy/header.php
@@ -13,6 +13,7 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+	<link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
