9945f42a9373f633f96d9f1670cb6add2a832a4b
Grey <greymcgowan@gmail.com>
2018-05-15 22:00:41 +1000

    added a list of all mobility aids to the top of the mobility aids page Completed initial styles on that list Centered the header/page Optimised layout for Smaller Monitors and Tablets.

--- wp-content/themes/szwarcbardpharmacy/mobilityaids/page_mobilityequipment.php
+++ wp-content/themes/szwarcbardpharmacy/mobilityaids/page_mobilityequipment.php
@@ -28,41 +28,56 @@ $container   = get_theme_mod( 'understrap_container_type' );
 				// creates the query and saves the content as $mobilityaid
 				$mobilityaid = new WP_Query($args);
 			?>
-			<?php
-				//the loop that repeats it for each post and displays it on the page
-				if ( $mobilityaid->have_posts() ):
-					while( $mobilityaid->have_posts() ) : $mobilityaid->the_post(); ?>
-						<article class="mobilityaidpost container">
-						<div class='row'>
-								<div class="col-lg-3">
-									<?php the_post_thumbnail(); ?> 
+			
+					<aside class="mobility" id="mobilityaside">
+								<h3>List of all mobility aids</h3>
+								<div class="mobilityaside">
+									<?php get_search_form(); ?>
+									<ul>
+										<?php while( $mobilityaid->have_posts() ) : $mobilityaid->the_post(); ?>
+											<li class="mobilitylist"><a href='<?php the_permalink(); ?>'><?php the_title(); ?></a></li>
+										<?php endwhile; ?>
+									</ul>
 								</div>
-								<div class="col-lg-9">
-									<h2 class="mobility"><?php the_title(); ?></h2>
-									<p class='purchasetype'> 
-										<span> <?php the_field('first_purchase_type');?> </span>
-											<?php if (get_field ('first_cost') ) : ?>
-										<span>$<?php echo the_field ('first_cost'); ?></span>
-											<?php endif; ?>
-									</p>
-									<p class='purchasetype'> 
-										<span> <?php the_field('second_purchase_type'); ?> </span>
-											<?php if (get_field ('second_cost') ) : ?>
-										<span>$<?php echo the_field ('second_cost'); ?></span>
-											<?php endif; ?>
-									</p>
-									<p class="mobilitycontent"><?php the_content(); ?> </p>
-									<p class='avaliable'> Is this item avalible? <?php the_field('in_stock'); ?> </p>
-									<?php if (get_field ('deposit_required') ) : ?>
-										<p class=mobilitydeposit>A deposit of $<?php echo the_field ('deposit'); ?> is required to rent this item</p>
-									<?php endif; ?>
-								</div>	
-							</div>
-						</article>
-					<?php endwhile; ?>
-				<?php else: ?>
-					<p> Sorry, we cant find any mobility equipment to list, please <a href='https://szwarcbardpharmacy.com.au/contact-us/'>contact us</a> </p>
-				<?php endif; ?>
+							</aside>
+					<?php
+						//the loop that repeats it for each post and displays it on the page
+						if ( $mobilityaid->have_posts() ):
+							while( $mobilityaid->have_posts() ) : $mobilityaid->the_post(); ?>
+								<div class="container">
+									<div class="row">	
+									<article class="mobpost">
+											<div class="col-lg-3">
+												<?php the_post_thumbnail(); ?> 
+											</div>
+											<div class="col-lg-9">
+												<h2 class="mobility"><a href='<?php the_permalink(); ?>'><?php the_title(); ?></a></h2>
+												<p class='purchasetype'> 
+													<span> <?php the_field('first_purchase_type');?> </span>
+														<?php if (get_field ('first_cost') ) : ?>
+													<span>$<?php echo the_field ('first_cost'); ?></span>
+														<?php endif; ?>
+												</p>
+												<p class='purchasetype'> 
+													<span> <?php the_field('second_purchase_type'); ?> </span>
+														<?php if (get_field ('second_cost') ) : ?>
+													<span>$<?php echo the_field ('second_cost'); ?></span>
+														<?php endif; ?>
+												</p>
+												<p class="mobilitycontent"><?php the_content(); ?> </p>
+												<p class='avaliable'> Is this item avalible? <?php the_field('in_stock'); ?> </p>
+												<?php if (get_field ('deposit_required') ) : ?>
+													<p class=mobilitydeposit>A deposit of $<?php echo the_field ('deposit'); ?> is required to rent this item</p>
+											</div>
+									</article>
+						<?php endif; ?>
+								<?php endwhile; ?>
+						<?php else: ?>
+								<p> Sorry, we cant find any mobility equipment to list, please <a href='https://szwarcbardpharmacy.com.au/contact-us/'>contact us</a> </p>
+						<?php endif; ?>
+							
+				</div>
+			</div>
 		</main>
 
 <?php
