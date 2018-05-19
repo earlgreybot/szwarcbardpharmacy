a0ebe78ab955113693106b41fe7a7f7db43ede1e
Grey <greymcgowan@gmail.com>
2018-05-19 22:30:34 +1000

    Created custom template for mobility aids single page #9 Styled the custom template #26 #29

---
+++ wp-content/themes/szwarcbardpharmacy/single-mobilityaid.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The template for displaying a single mobility aid
+ *
+ * @package Szwarcbard_Pharmacy
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+		
+		<?php
+		while ( have_posts() ) :
+			the_post();
+		?>
+
+			<div class="container">
+									<div class="row">	
+									<article class="mobpost">
+											<div class="col-lg-3">
+												<?php the_post_thumbnail(); ?> 
+											</div>
+											<div class="col-lg-9">
+												<h2 class="mobility"><?php the_title(); ?></h2>
+												<p class='purchasetype'> 
+													<span> <?php the_field('first_purchase_type');?> </span>
+														<?php if (get_field ('first_cost') ) : ?>
+													<span>$<?php echo the_field ('first_cost'); ?></span>
+														<?php endif; ?>
+												</p>
+												<p class='purchasetype'> 
+													<span> <?php the_field('second_purchase_type'); ?> </span>
+														<?php if (get_field ('second_cost') ) : ?>
+													<span>$<?php echo the_field ('second_cost'); ?></span>
+														<?php endif; ?>
+												</p>
+												<p class="mobilitycontent"><?php the_content(); ?> </p>
+												<p class='avaliable'> Is this item avalible? <?php the_field('in_stock'); ?> </p>
+												<?php if (get_field ('deposit_required') ) : ?>
+													<p class=mobilitydeposit>A deposit of $<?php echo the_field ('deposit'); ?> is required to rent this item</p>
+												<p id=contactus> In order to rent or buy this item, <a href=https://szwarcbardpharmacy.com.au/contact-us/> visit us in store, email us or call us to discuss options </a></p>	
+											
+											</div>
+									</article>
+						<?php endif; ?>
+			
+		<?php	
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
